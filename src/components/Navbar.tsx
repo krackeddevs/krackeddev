07a91b034dd3be5c3438cf3745b5162113f1cabd
fadlikhalid <fadlikhalid91@gmail.com>
2025-12-24 19:35:35 +0800

    perf: mark scroll listener as passive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 20);
     };
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll, { passive: true });
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
